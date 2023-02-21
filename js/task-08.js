3eb93dfcf94d7c59e7c7439e7381954f1aac5070
Nataliia Samokhvalova <samohvalova520@gmail.com>
2023-02-21 16:37:32 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -5,15 +5,14 @@ form.addEventListener('submit', onSubmitForm);
 function onSubmitForm (event) {
     event.preventDefault();
 
-    const formElements = event.currentTarget.elements;
-    const email = formElements.email.value;
-    const password = formElements.password.value;
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
 
-    if (email === '' || password === '') {
-        alert('Всі поля повинні бути заповнені');
+    if (email.value === '' || password.value === '') {
+        return alert('Всі поля повинні бути заповнені');
     } 
 
-    if (email !== '' && password !== '') {
         const formData = new FormData(event.currentTarget);
         console.log(formData);
         event.currentTarget.reset();
@@ -22,5 +21,4 @@ function onSubmitForm (event) {
         console.log('name', name);
         console.log('value', value);
         });
-    }
 }
